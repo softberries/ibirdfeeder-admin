0b9b00001d8a1f3954935385c2e48445f9118634
softberries <krzysztof.grajek@googlemail.com>
2016-05-21 08:23:36 +0200

    first commit

---
+++ src/main/java/com/softwarepassion/ibirdfeeder/aws/s3/S3Manager.java
@@ -0,0 +1,43 @@
+package com.softwarepassion.ibirdfeeder.aws.s3;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class S3Manager {
+
+    private static final Logger log = LoggerFactory.getLogger(S3Manager.class);
+
+    private static final String S3_BASE_URL = "https://s3-eu-west-1.amazonaws.com/birdfeeder001/";
+    private static final String BIRD_FEEDER_BUCKET_NAME = "birdfeeder001";
+    private final AmazonS3 s3;
+
+    public S3Manager() {
+        s3 = new AmazonS3Client();
+    }
+
+    public List<String> listBucketContent() {
+
+        ObjectListing listing = s3.listObjects( BIRD_FEEDER_BUCKET_NAME, "small" );
+        List<S3ObjectSummary> summaries = listing.getObjectSummaries();
+        List<String> urls = new ArrayList<>();
+        while (listing.isTruncated()) {
+            listing = s3.listNextBatchOfObjects (listing);
+            summaries.addAll (listing.getObjectSummaries());
+        }
+        for (S3ObjectSummary objectSummary : summaries) {
+            if (objectSummary.getKey().startsWith("small_image_")) {
+                urls.add(S3_BASE_URL + objectSummary.getKey());
+            }
+        }
+        log.info("Found " + urls.size() + " files.");
+        return urls;
+    }
+}
